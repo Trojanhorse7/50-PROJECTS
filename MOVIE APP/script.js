3a9f95884576b57251a375c45065e579bc8017c7
Trojanhorse7 <felixgogodae777@gmail.com>
2022-06-23 14:47:10 +0100

    MOVIE APP

--- MOVIE APP/script.js
+++ MOVIE APP/script.js
@@ -18,17 +18,22 @@ async function getMovies(url) {
 }
 
 function showMovies(movies) {
+    main.innerHTML = ''
 
-    movies.forEach((movie)=> {
+    movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie
+
         const movieEl = document.createElement("div") 
         movieEl.classList.add("movie")
+
         movieEl.innerHTML = `
-            <img src="${IMG_PATH + poster_path}" alt="${title}>
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
+
             <div class="overview">
                 <h3>Overview</h3>
                 ${overview}
